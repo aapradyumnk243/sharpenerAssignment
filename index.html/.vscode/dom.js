bfba91558138b63086e62916965a61fcb0ae7c28
Aayush kripa <upadhyaypradyumn272204@gmail.com>
2023-01-26 06:24:30 +0530

    all chamges made

---
+++ index.html/.vscode/dom.js
@@ -0,0 +1,36 @@
+//EXAMINE THE DOCUMENT OBJEST//
+
+// //console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title=123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// //document.all[10].textContent="Hello";con
+// // console.log(document.forms[0]);
+// //console.log(document.links);
+//console.log(document.images);
+
+
+//GETELEMENTEDBYID //
+//console.log(document.getElementById('header-title'));
+//var headertitle=  document.getElementById('header-title');
+var header=  document.getElementById('main-header');
+//console.log(headertitle);
+//headertitle.textContent="Hello";
+//headertitle.innerText= "Goodbye";
+//console.log(headertitle.textContent);
+//console.log(headertitle.innerText);
+//headerTitle.innerHTML = '<h3>Hello</h3>';
+header.style.borderBottom = "solid 3px #000";
+//var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1])
+//items[1].textContent= 'Hello 2';
+//item[1].style.fontweight= 'bold';
+
+
