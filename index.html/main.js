ce42773cdf16fed7d2841604dafb39a3399fc7ce
Aayush kripa <upadhyaypradyumn272204@gmail.com>
2023-02-06 00:36:25 +0530

    added to local storage

---
+++ index.html/main.js
@@ -0,0 +1,9 @@
+//Retrieving data
+var nam = document.getElementById("name");
+var eml = document.getElementById("email");
+
+//storing data
+var user = localStorage.setItem("nam",nam);
+var mail = localStorage.setItem("eml",eml);
+
+
